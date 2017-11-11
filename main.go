44d8c7fa24b7105de74756c75b3613cf82a59df1
Napat <inotecpe25@gmail.com>
2017-11-12 01:56:24 +0700

    add load data to 1d array

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -63,27 +64,57 @@ func buildTrainingSet() {
 		png.Encode(outfile, gray)
 	}
 }
+func get1DArrayImage(imagePath string) []float64 {
+	output := make([]float64, 10000)
 
+	infile, err := os.Open(imagePath)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer infile.Close()
+	src, _, err := image.Decode(infile)
+	if err != nil {
+		panic(err.Error())
+	}
+	bounds := src.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	indexCount := 0
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			oldColor := src.At(x, y)
+			r, g, b, _ := oldColor.RGBA()
+			// fmt.Println(r)
+
+			avg := 0.2125*float64(r) + 0.7154*float64(g) + 0.0721*float64(b)
+			// fmt.Println(avg)
+			output[indexCount] = float64(uint8(math.Ceil(avg))) / 255.0
+			indexCount++
+		}
+	}
+	return output
+}
 func main() {
 	/* create some random noise from ./input directory to ./output directory */
 	buildTrainingSet()
-	// inputImage0 := {}
+	inputImage0 := get1DArrayImage("./output/0.png")
+	fmt.Println("load 0.png done")
+	inputImage1 := get1DArrayImage("./output/1.png")
+	fmt.Println("load 1.png done")
+	inputImage2 := get1DArrayImage("./output/2.png")
+	fmt.Println("load 2.png done")
+	inputImage3 := get1DArrayImage("./output/3.png")
+	fmt.Println("load 3.png done")
+	fmt.Printf("%+v\n", inputImage0)
 	patterns := [][][]float64{
-		{{0}, {0, 0, 0, 0, 0, 0, 0}},
-		{{1}, {0, 1, 1, 0, 0, 0, 0}},
-		{{2}, {1, 1, 0, 1, 1, 0, 1}},
-		{{3}, {1, 1, 1, 1, 0, 0, 1}},
-		{{4}, {0, 1, 1, 0, 0, 1, 1}},
-		{{5}, {1, 0, 1, 1, 0, 1, 1}},
-		{{6}, {1, 0, 1, 1, 1, 1, 1}},
-		{{7}, {1, 1, 1, 0, 0, 0, 0}},
-		{{8}, {1, 1, 1, 1, 1, 1, 1}},
-		{{9}, {1, 1, 1, 1, 0, 1, 1}},
+		{inputImage0, {0, 0, 0, 0, 0, 0, 0}},
+		{inputImage1, {0, 1, 1, 0, 0, 0, 0}},
+		{inputImage2, {1, 1, 0, 1, 1, 0, 1}},
+		{inputImage3, {1, 1, 1, 1, 0, 0, 1}},
 	}
 
 	// // Training Phase
 	ff := &gobrain.FeedForward{}
-	ff.Init(1, 4, 7)
+	ff.Init(10000, 10000, 10000)
 	ff.Train(patterns, 1000, 0.6, 0.4, false)
 
 	// Testing Phase
